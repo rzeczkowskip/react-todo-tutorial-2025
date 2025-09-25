9c335b17200d6270d1d4f9976f3b6c22310a6047
Piotr Rzeczkowski <przeczkowski@kaliop.com>
2025-09-25 20:43:11 +0200

    Add form handler

--- react-demo/src/App.jsx
+++ react-demo/src/App.jsx
@@ -5,12 +5,26 @@ const App = () => {
     { id: 3, text: 'Szamka', done: false }, // ← new element
   ];
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const data = new FormData(e.target);
+
+    items.push({
+      id: items.length + 1,
+      text: data.get('task'),
+      done: false,
+    });
+
+    // clear form inputs
+    e.target.reset();
+  };
+
   return (
     <main>
       <h1>Do zrobienia</h1>
 
-      <form>
-        <input placeholder="Co chcesz zrobić?" />
+      <form onSubmit={handleSubmit}>
+        <input placeholder="Co chcesz zrobić?" name="task" />
         <button>Dodaj</button>
       </form>
 
