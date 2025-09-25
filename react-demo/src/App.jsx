a92405b6acff734291e2bdb453aa072ff49b5111
Piotr Rzeczkowski <przeczkowski@kaliop.com>
2025-09-25 20:41:14 +0200

    Solve part1 exercise

--- react-demo/src/App.jsx
+++ react-demo/src/App.jsx
@@ -2,6 +2,7 @@ const App = () => {
   const items = [
     { id: 1, text: 'Nauka React', done: false },
     { id: 2, text: 'Spacer z psem', done: true },
+    { id: 3, text: 'Szamka', done: false }, // ← new element
   ];
 
   return (
