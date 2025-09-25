c23f49c5876228ced0d9b7aa8f35324a0ab2f36b
Piotr Rzeczkowski <przeczkowski@kaliop.com>
2025-09-25 20:40:32 +0200

    Create simple todo list

--- react-demo/src/App.jsx
+++ react-demo/src/App.jsx
@@ -1,35 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  const items = [
+    { id: 1, text: 'Nauka React', done: false },
+    { id: 2, text: 'Spacer z psem', done: true },
+  ];
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <main>
+      <h1>Do zrobienia</h1>
+      <ul>
+        {items.map((t) => (
+          <li key={t.id}>
+            <input type="checkbox" defaultChecked={t.done} disabled /> {t.text}
+          </li>
+        ))}
+      </ul>
+    </main>
+  );
+};
 
-export default App
+export default App;
